d78144cfb5accf876e07880c2beb804cfcf1f5ed
Daniela Tomás <daniela.tomas@talkdesk.com>
2025-02-05 15:09:14 +0000

    section 5 - create Post beer

--- src/main/java/guru/springframework/spring6restmvc/controler/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controler/BeerController.java
@@ -4,6 +4,8 @@ import guru.springframework.spring6restmvc.services.BeerService;
 import guru.springframework.spring6restmvc.model.Beer;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +18,15 @@ import java.util.UUID;
 @RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
+
+    @PostMapping
+    //@RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity handlePost(@RequestBody Beer beer){
+
+        Beer savedBeer = beerService.saveNewBeer(beer);
+
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
 
     @GetMapping()
     public List<Beer> listBeers(){
