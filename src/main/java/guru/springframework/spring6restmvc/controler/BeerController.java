eae16c765a939aa9480b1f47b196af856bd48b27
Daniela Tomás <daniela.tomas@talkdesk.com>
2025-02-05 14:54:26 +0000

    section 5 - get customer requests

--- src/main/java/guru/springframework/spring6restmvc/controler/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controler/BeerController.java
@@ -5,8 +5,7 @@ import guru.springframework.spring6restmvc.model.Beer;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -14,18 +13,20 @@ import java.util.UUID;
 @Slf4j
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beer")
+    @GetMapping()
     public List<Beer> listBeers(){
         return beerService.listBeers();
     }
 
-    public Beer getBeerById(UUID id){
+    @GetMapping(value = "{beerId}")
+    public Beer getBeerById(@PathVariable("beerId") UUID beerId){
 
         log.debug("Get Beer by Id - in controller");
 
-        return beerService.getBeerById(id);
+        return beerService.getBeerById(beerId);
     }
 }
